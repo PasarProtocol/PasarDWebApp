08cd9af7b1d11aebc33f954043937186938edcd7
fullstack12345 <leo.hhu5731@gmail.com>
2021-12-26 21:15:39 -0800

    click card and address to show more details in explorer page

--- src/components/explorer/CollectionView/LatestTransactions.js
+++ src/components/explorer/CollectionView/LatestTransactions.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import { formatDistance } from 'date-fns';
-import { Box, Stack, Link, Typography } from '@mui/material';
+import { Icon } from '@iconify/react';
+import { Box, Stack, Link, Typography, IconButton } from '@mui/material';
+import externalLinkFill from '@iconify/icons-eva/external-link-fill';
 import palette from '../../../theme/palette'
 
 // material
@@ -14,19 +16,30 @@ TransItem.propTypes = {
   isLast: PropTypes.bool.isRequired
 };
 function TransItem({ trans, isLast }) {
-  const { image, title, description, postedAt, gasFee } = trans;
+  const { image, tokenId, txhash, description, postedAt, gasFee } = trans;
   const sx = isLast?{}:{borderBottom: '1px solid', borderColor: palette.light.grey['300'], pb: 2};
   return (
       <Stack direction="row" alignItems="center" spacing={2} sx={sx}>
-          <Box
-              component="img"
-              alt={title}
-              src={image}
-              sx={{ width: 48, height: 48, borderRadius: 1 }}
-          />
+          <Link href={`/explorer/transaction/${tokenId}`} underline="none" sx={{borderRadius: 1}} >
+            <Box
+                component="img"
+                alt=""
+                src={image}
+                onError={(e) => e.target.src = '/static/broken-image.svg'}
+                sx={{
+                  width: 48, height: 48, borderRadius: 1, cursor: 'pointer', transition: 'transform .5s ease-out',
+                  '&:hover': { transform: 'scale(1.2)' }
+                }}
+            />
+          </Link>
           <Box sx={{ minWidth: 0, flexGrow: 1 }}>
               <Typography variant="body2" color='text.secondary' noWrap>
-                  {title}
+                <Link href={`https://esc.elastos.io/tx/${txhash}`} sx={{borderRadius: 1}} target="_blank">
+                  Tx Hash : {reduceHexAddress(txhash)}
+                  <IconButton type="button" sx={{ p: '5px' }} aria-label="link">
+                      <Icon icon={externalLinkFill} width="17px"/>
+                  </IconButton>
+                </Link>
               </Typography>
               <Typography variant="body2" color='text.secondary' noWrap>
                   Method: <MethodLabel description={description}/>
@@ -47,14 +60,15 @@ export default function LatestTransactions(props) {
   return (
     <CollectionView title={props.title} to="transaction">
       {props.isLoading && <LoadingScreen />}
-      {props.dataList.map((collectible, index) => (
+      {props.dataList.map((trans, index) => (
           <TransItem 
             key={index}
             trans={{
-              image: getThumbnail(collectible.thumbnail),
-              title: "Tx Hash : ".concat(reduceHexAddress(collectible.tokenIdHex)),
-              gasFee: collectible.royalties / 10 ** 8,
-              postedAt: collectible.createTime*1000,
+              image: getThumbnail(trans.thumbnail),
+              tokenId: trans.tokenIdHex,
+              txhash: trans.tokenIdHex,
+              gasFee: trans.royalties / 10 ** 8,
+              postedAt: trans.createTime*1000,
               description: 'Transfer'
             }}
             isLast={index===props.dataList.length-1}
--- src/components/explorer/CollectionView/NewestCollectibles.js
+++ src/components/explorer/CollectionView/NewestCollectibles.js
@@ -1,22 +1,72 @@
+import PropTypes from 'prop-types';
+import { formatDistance } from 'date-fns';
 // material
+import { Box, Stack, Link, Typography } from '@mui/material';
 import CollectionView from './Template'
-import NewsItem from './NewsItem'
 import LoadingScreen from '../../LoadingScreen';
-import { reduceHexAddress, getElapsedTime, getThumbnail } from '../../../utils/common';
+import { reduceHexAddress, getThumbnail } from '../../../utils/common';
+import CopyButton from '../../CopyButton';
 // ----------------------------------------------------------------------
+
+
+CollectibleItem.propTypes = {
+  news: PropTypes.object.isRequired,
+  isLast: PropTypes.bool.isRequired
+};
+
+function CollectibleItem({ news, isLast, sx }) {
+  const { image, title, tokenId, postedAt, creator } = news;
+  const style = isLast?{...sx}:{borderBottom: '1px solid', borderColor: 'grey.300', pb: 2, ...sx};
+  return (
+      <Stack direction="row" alignItems="center" spacing={2} sx={style}>
+          <Link href={`/explorer/collectible/detail/${tokenId}`} underline="none" sx={{borderRadius: 1}} >
+            <Box
+                component="img"
+                alt={title}
+                src={image}
+                onError={(e) => e.target.src = '/static/broken-image.svg'}
+                sx={{
+                  width: 48, height: 48, borderRadius: 1, cursor: 'pointer', transition: 'transform .5s ease-out',
+                  '&:hover': { transform: 'scale(1.2)' }
+                }}
+            />
+          </Link>
+          <Box sx={{ minWidth: 0, flexGrow: 1 }}>
+              <Typography color="inherit" variant="subtitle2" noWrap>
+                  {title}
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+                <Link href={`/explorer/transaction/${creator}`} sx={{borderRadius: 1}} >
+                  Creator : {reduceHexAddress(creator)}
+                </Link>
+                <CopyButton text={creator}/>
+              </Typography>
+          </Box>
+          <Box>
+              <Typography variant="body2" sx={{ flexShrink: 0, color: 'text.secondary' }} align="right" noWrap>
+                  {formatDistance(postedAt, new Date(), { addSuffix: true }).replace("about","").trim()}
+              </Typography>
+              <Typography variant="body2" sx={{ flexShrink: 0, color: 'text.secondary' }} align="right" noWrap>
+                  Token ID : {reduceHexAddress(tokenId)}
+              </Typography>
+          </Box>
+      </Stack>
+  );
+}
+
 export default function NewestCollectibles(props) {
   return (
     <CollectionView title={props.title} to="collectible">
       {props.isLoading && <LoadingScreen />}
       {props.dataList.map((collectible, index) => (
-          <NewsItem 
+          <CollectibleItem 
             key={index}
             news={{
               image: getThumbnail(collectible.thumbnail),
               title: collectible.name,
-              creator: "Creator : ".concat(reduceHexAddress(collectible.holder)),
+              creator: collectible.holder,
               postedAt: collectible.createTime*1000,
-              description: "Token ID : ".concat(reduceHexAddress(collectible.tokenIdHex))
+              tokenId: collectible.tokenIdHex
             }}
             isLast={index===props.dataList.length-1}
           />
