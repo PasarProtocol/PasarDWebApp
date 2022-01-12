483e1bc61e541a7fc35df5fbf4808c345705fe50
fullstack12345 <leo.hhu5731@gmail.com>
2022-01-12 06:47:03 -0800

    Divide explorer view into sub components

---
+++ src/components/marketplace/AssetFilterPan.js
@@ -0,0 +1,176 @@
+import React from 'react';
+// material
+import { styled } from '@mui/material/styles';
+import { Box, Accordion, AccordionSummary, AccordionDetails, Grid, MenuItem, List, TextField, FormControlLabel, Typography, Stack, 
+  ListItemButton, ListItemIcon, ListItemText, Select, Drawer, Divider, Button} from '@mui/material';
+import { Icon } from '@iconify/react';
+import arrowIosDownwardFill from '@iconify/icons-eva/arrow-ios-downward-fill';
+
+import CustomSwitch from '../custom-switch';
+import SearchBox from '../SearchBox';
+import Scrollbar from '../Scrollbar';
+// ----------------------------------------------------------------------
+const DrawerStyle = styled(Drawer)(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    display: 'none'
+  },
+  '& .MuiDrawer-paper': {
+    boxSizing: 'border-box',
+    transition: theme.transitions.create(['top'], {
+      easing: theme.transitions.easing.easeInOut,
+      duration: theme.transitions.duration.standard
+    }),
+  }
+}));
+
+export default function AssetFilterPan(props){
+  const {sx, scrollMaxHeight, btnNames, selectedBtns, handleBtnFunc} = props
+  return(
+    <DrawerStyle
+      variant="persistent"
+      open
+      sx={sx}
+    >
+      <Scrollbar sx={{maxHeight: scrollMaxHeight}}>
+        <Grid container width="100%">
+          <Grid item md={12}>
+            <Accordion
+              defaultExpanded={1&&true}
+            >
+              <AccordionSummary expandIcon={<Icon icon={arrowIosDownwardFill} width={20} height={20}/>} sx={{px: 4}}>
+                <Typography variant="body2">Status</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {
+                  [...btnNames].splice(0,3).map((name, index)=>(
+                    <Button key={index} variant={selectedBtns.includes(index)?"contained":"outlined"} color="primary" onClick={()=>handleBtnFunc(index)}>
+                      {name}
+                    </Button>
+                  ))
+                }
+              </AccordionDetails>
+            </Accordion>
+            <Divider />
+          </Grid>
+          <Grid item md={12}>
+            <Accordion
+              defaultExpanded={1&&true}
+            >
+              <AccordionSummary expandIcon={<Icon icon={arrowIosDownwardFill} width={20} height={20}/>} sx={{px: 4}}>
+                <Typography variant="body2">Price Range</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Stack spacing={1}>
+                  <Select
+                    defaultValue={0}
+                    // value={selected}
+                    // onChange={handleChange}
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    size="small"
+                    sx={{
+                      mr: 1,
+                      width: '100%',
+                      '& .MuiListItemText-root, & .MuiListItemText-root>span': {
+                        display: 'inline'
+                      }
+                    }}
+                  >
+                    <MenuItem value={0}>
+                      <ListItemIcon>
+                        <Box component="img" src="/static/elastos.svg" sx={{ width: 18, display: 'inline' }} />
+                      </ListItemIcon>
+                      <ListItemText primary="Elastos (ELA)" />
+                    </MenuItem>
+                  </Select>
+                  <Grid container>
+                    <Grid item md={5}>
+                      <TextField label="Min" size="small"/>
+                    </Grid>
+                    <Grid item md={2} align="center">
+                      <Typography variant="body2" sx={{pt: 1}}>to</Typography>
+                    </Grid>
+                    <Grid item md={5}>
+                      <TextField label="Max" size="small"/>
+                    </Grid>
+                  </Grid>
+                  <Button variant="contained" color="primary" width="100%">
+                    Apply
+                  </Button>
+                </Stack>
+              </AccordionDetails>
+            </Accordion>
+            <Divider />
+          </Grid>
+          <Grid item md={12}>
+            <Accordion
+              defaultExpanded={1&&true}
+            >
+              <AccordionSummary expandIcon={<Icon icon={arrowIosDownwardFill} width={20} height={20}/>} sx={{px: 4}}>
+                <Typography variant="body2">Collections</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <SearchBox sx={{width: '100%'}} rootsx={{px: '0 !important', mb: 1}} placeholder="Search collections"/>
+                <Scrollbar sx={{maxHeight: 200}}>
+                  <List
+                    sx={{ width: '100%', bgcolor: 'background.paper' }}
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                  >
+                    {
+                      [...Array(10)].map((value, id)=>(
+                          <ListItemButton key={id}>
+                            <ListItemIcon>
+                              <Box draggable = {false} component="img" src="/static/feeds-sticker.svg" sx={{ width: 24, height: 24, borderRadius: 2, p: .5, backgroundColor: 'black' }} />
+                            </ListItemIcon>
+                            <ListItemText primary="Feeds NFT Sticker" />
+                          </ListItemButton>
+                      ))
+                    }
+                  </List>
+                </Scrollbar>
+              </AccordionDetails>
+            </Accordion>
+            <Divider />
+          </Grid>
+        </Grid>
+        <Grid item md={12}>
+            <Accordion
+              defaultExpanded={1&&true}
+            >
+              <AccordionSummary expandIcon={<Icon icon={arrowIosDownwardFill} width={20} height={20}/>} sx={{px: 4}}>
+                <Typography variant="body2">Item Type</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                {
+                  [...btnNames].splice(3).map((name, index)=>(
+                    <Button key={index} variant={selectedBtns.includes(index+3)?"contained":"outlined"} color="primary" onClick={()=>handleBtnFunc(index+3)}>
+                      {name}
+                    </Button>
+                  ))
+                }
+              </AccordionDetails>
+            </Accordion>
+            <Divider />
+          </Grid>
+        <Grid item md={12}>
+          <Accordion
+            defaultExpanded={1&&true}
+          >
+            <AccordionSummary expandIcon={<Icon icon={arrowIosDownwardFill} width={20} height={20}/>} sx={{px: 4}}>
+              <Typography variant="body2">Explicit & Sensitive Content</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <FormControlLabel
+                control={<CustomSwitch onChange={()=>{}}/>}
+                label="Off"
+                labelPlacement="end"
+                sx={{ml:2, pr:2}}
+              />
+            </AccordionDetails>
+          </Accordion>
+          <Divider />
+        </Grid>
+      </Scrollbar>
+    </DrawerStyle>
+  )
+}
---
+++ src/components/marketplace/AssetGrid.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+// material
+import { Box } from '@mui/material';
+
+import AssetCard from './AssetCard';
+import { getThumbnail } from '../../utils/common';
+// ----------------------------------------------------------------------
+const StackedGrid = ({
+  gridItemWidth = "280px",
+  children,
+  ...props
+}) => (
+  <Box display="grid" gridTemplateColumns="repeat(auto-fill, minmax(300px, 1fr))" gap={1.5}>
+    {children}
+  </Box>
+);
+const GridItems = (props) => (
+    <AnimatePresence>
+      {props.items.map((item, index) => (
+        <motion.div
+          key={index}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <AssetCard
+            ratio={props.ratio}
+            key={index}
+            id={item}
+            index={index}
+            mb={props.rowGap}
+            thumbnail={getThumbnail(item.asset)}
+            title={item.name && item.name}
+            url={props.url}
+            {...props}
+          />
+        </motion.div>
+      ))}
+    </AnimatePresence>
+);
+export default function AssetGrid(props){
+  return(
+    <StackedGrid>
+      <GridItems items={props.assets} ratio="3:2" />
+    </StackedGrid>
+  )
+}
