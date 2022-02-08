f6dee829f317d7aa2be3105a86b2fb5ff670c52c
fullstack12345 <leo.hhu5731@gmail.com>
2022-02-08 15:57:55 +0900

    feat: Add GitHub logo and link to footer

--- src/layouts/main/MainFooter.js
+++ src/layouts/main/MainFooter.js
@@ -5,13 +5,13 @@ import telegramIcon from '@iconify/icons-ic/round-telegram';
 // material
 import { styled } from '@mui/material/styles';
 import { Grid, Link, Divider, Container, Typography, IconButton, Stack, Box } from '@mui/material';
-import palette from '../../theme/palette'
-import ModeSwitch from '../../components/mode-switch'
+import palette from '../../theme/palette';
+import ModeSwitch from '../../components/mode-switch';
 // ----------------------------------------------------------------------
 
 const SOCIALS = [
   { name: 'TelegramIcon', icon: telegramIcon },
-  { name: 'Twitter', icon: twitterFill },
+  { name: 'Twitter', icon: twitterFill }
 ];
 
 const RootStyle = styled('div')(({ theme, sx }) => ({
@@ -20,7 +20,7 @@ const RootStyle = styled('div')(({ theme, sx }) => ({
   bottom: 0,
   width: '100%',
   // position: 'relative',
-  backgroundColor: "#c4c4c4",
+  backgroundColor: '#c4c4c4',
   [theme.breakpoints.down('md')]: {
     display: 'none'
   }
@@ -43,10 +43,9 @@ function CopyRight(prop) {
     </Typography>
   );
 }
-export default function MainFooter({hidden, isHome}) {
-  const sx=hidden?{display: 'none'}:{}
-  if(isHome)
-    sx.backgroundColor = '#0d0d0d'
+export default function MainFooter({ hidden, isHome }) {
+  const sx = hidden ? { display: 'none' } : {};
+  if (isHome) sx.backgroundColor = '#0d0d0d';
   return (
     <RootStyle sx={sx}>
       <Divider />
@@ -57,13 +56,21 @@ export default function MainFooter({hidden, isHome}) {
           sx={{ textAlign: { xs: 'center', md: 'left' } }}
         >
           <Grid item xs={12} md={6}>
-            <h3 style={{paddingBottom: 15}}>
-              <span role="img" aria-label=''>⚡ </span>Powered by Elastos
-              <Box draggable = {false} component="img" src="/static/elastos.svg" sx={{ width: 21, display: 'inline', pl: .5 }} />
+            <h3 style={{ paddingBottom: 15 }}>
+              <span role="img" aria-label="">
+                ⚡{' '}
+              </span>
+              Powered by Elastos
+              <Box
+                draggable={false}
+                component="img"
+                src="/static/elastos.svg"
+                sx={{ width: 21, display: 'inline', pl: 0.5 }}
+              />
             </h3>
             <Typography variant="body2" sx={{ pr: { md: 5 }, color: 'GrayText' }}>
-              Pasar Protocol is a truly decentralized Web3.0 Marketplace, Collectible Explorer
-              and Analytics Platform for NFTs created on the Elastos Smart Chain (ESC)
+              Pasar Protocol is a truly decentralized Web3.0 Marketplace, Collectible Explorer and Analytics Platform
+              for NFTs created on the Elastos Smart Chain (ESC)
             </Typography>
           </Grid>
 
@@ -79,14 +86,41 @@ export default function MainFooter({hidden, isHome}) {
                   <Icon icon={social.icon} width={40} height={40} />
                 </IconButton>
               ))} */}
-              <IconButton color="inherit" sx={{ p: '2px' }} href="https://discord.gg/RPbcBv8ckh" component={Link} target="_blank">
+              <IconButton
+                color="inherit"
+                sx={{ p: '2px' }}
+                href="https://discord.gg/RPbcBv8ckh"
+                component={Link}
+                target="_blank"
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="36" height="36" viewBox="0 0 256 256">
                   <g transform="translate(128 128) scale(0.72 0.72)">
                     <g transform="translate(-175.05 -175.05000000000004) scale(3.89 3.89)">
-                      <path d="M 45 0 C 20.147 0 0 20.147 0 45 c 0 24.853 20.147 45 45 45 s 45 -20.147 45 -45 C 90 20.147 69.853 0 45 0 z M 56.901 62.98 c 0 0 -1.541 -1.841 -2.825 -3.467 c 5.608 -1.584 7.748 -5.094 7.748 -5.094 c -1.755 1.156 -3.425 1.969 -4.923 2.526 c -2.14 0.899 -4.195 1.498 -6.207 1.841 c -4.11 0.771 -7.877 0.557 -11.087 -0.043 c -2.44 -0.471 -4.538 -1.156 -6.293 -1.841 c -0.985 -0.385 -2.055 -0.856 -3.125 -1.455 c -0.128 -0.086 -0.257 -0.128 -0.385 -0.214 c -0.086 -0.043 -0.128 -0.086 -0.171 -0.128 c -0.771 -0.428 -1.199 -0.728 -1.199 -0.728 s 2.055 3.425 7.491 5.051 c -1.284 1.627 -2.868 3.553 -2.868 3.553 C 23.596 62.68 20 56.473 20 56.473 c 0 -13.784 6.164 -24.957 6.164 -24.957 c 6.164 -4.623 12.029 -4.495 12.029 -4.495 l 0.428 0.514 c -7.706 2.226 -11.259 5.608 -11.259 5.608 s 0.942 -0.514 2.526 -1.241 c 4.581 -2.012 8.219 -2.568 9.717 -2.697 c 0.257 -0.043 0.471 -0.086 0.728 -0.086 c 2.611 -0.342 5.565 -0.428 8.647 -0.086 c 4.067 0.471 8.433 1.669 12.885 4.11 c 0 0 -3.382 -3.211 -10.659 -5.437 l 0.599 -0.685 c 0 0 5.865 -0.128 12.029 4.495 c 0 0 6.164 11.173 6.164 24.957 C 70 56.473 66.361 62.68 56.901 62.98 z" transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round" style={{fill: 'currentColor'}}/>
-                      <path d="M 36.995 42.988 c -2.44 0 -4.366 2.14 -4.366 4.752 c 0 2.611 1.969 4.752 4.366 4.752 c 2.44 0 4.366 -2.14 4.366 -4.752 C 41.404 45.129 39.435 42.988 36.995 42.988 z M 52.62 42.988 c -2.44 0 -4.366 2.14 -4.366 4.752 c 0 2.611 1.969 4.752 4.366 4.752 c 2.44 0 4.366 -2.14 4.366 -4.752 C 56.986 45.129 55.06 42.988 52.62 42.988 z" transform=" matrix(1 0 0 1 0 0) " strokeLinecap="round" style={{fill: 'currentColor'}}/>
+                      <path
+                        d="M 45 0 C 20.147 0 0 20.147 0 45 c 0 24.853 20.147 45 45 45 s 45 -20.147 45 -45 C 90 20.147 69.853 0 45 0 z M 56.901 62.98 c 0 0 -1.541 -1.841 -2.825 -3.467 c 5.608 -1.584 7.748 -5.094 7.748 -5.094 c -1.755 1.156 -3.425 1.969 -4.923 2.526 c -2.14 0.899 -4.195 1.498 -6.207 1.841 c -4.11 0.771 -7.877 0.557 -11.087 -0.043 c -2.44 -0.471 -4.538 -1.156 -6.293 -1.841 c -0.985 -0.385 -2.055 -0.856 -3.125 -1.455 c -0.128 -0.086 -0.257 -0.128 -0.385 -0.214 c -0.086 -0.043 -0.128 -0.086 -0.171 -0.128 c -0.771 -0.428 -1.199 -0.728 -1.199 -0.728 s 2.055 3.425 7.491 5.051 c -1.284 1.627 -2.868 3.553 -2.868 3.553 C 23.596 62.68 20 56.473 20 56.473 c 0 -13.784 6.164 -24.957 6.164 -24.957 c 6.164 -4.623 12.029 -4.495 12.029 -4.495 l 0.428 0.514 c -7.706 2.226 -11.259 5.608 -11.259 5.608 s 0.942 -0.514 2.526 -1.241 c 4.581 -2.012 8.219 -2.568 9.717 -2.697 c 0.257 -0.043 0.471 -0.086 0.728 -0.086 c 2.611 -0.342 5.565 -0.428 8.647 -0.086 c 4.067 0.471 8.433 1.669 12.885 4.11 c 0 0 -3.382 -3.211 -10.659 -5.437 l 0.599 -0.685 c 0 0 5.865 -0.128 12.029 4.495 c 0 0 6.164 11.173 6.164 24.957 C 70 56.473 66.361 62.68 56.901 62.98 z"
+                        transform=" matrix(1 0 0 1 0 0) "
+                        strokeLinecap="round"
+                        style={{ fill: 'currentColor' }}
+                      />
+                      <path
+                        d="M 36.995 42.988 c -2.44 0 -4.366 2.14 -4.366 4.752 c 0 2.611 1.969 4.752 4.366 4.752 c 2.44 0 4.366 -2.14 4.366 -4.752 C 41.404 45.129 39.435 42.988 36.995 42.988 z M 52.62 42.988 c -2.44 0 -4.366 2.14 -4.366 4.752 c 0 2.611 1.969 4.752 4.366 4.752 c 2.44 0 4.366 -2.14 4.366 -4.752 C 56.986 45.129 55.06 42.988 52.62 42.988 z"
+                        transform=" matrix(1 0 0 1 0 0) "
+                        strokeLinecap="round"
+                        style={{ fill: 'currentColor' }}
+                      />
                     </g>
                   </g>
+                </svg>
+              </IconButton>
+              <IconButton
+                color="inherit"
+                sx={{ p: '0x', m: '0px', background: '#FFF', textDecoration: "none" }}
+                href="https://github.com/PasarProtocol"
+                component={Link}
+                target="_blank"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
                 </svg>
               </IconButton>
             </Stack>
@@ -100,17 +134,24 @@ export default function MainFooter({hidden, isHome}) {
         <Grid
           container
           justifyContent={{ xs: 'center', md: 'space-between' }}
-          sx={{ textAlign: { xs: 'center', md: 'left' }, mt: 2, borderTop: '1px solid', borderColor: palette.light.grey['300']}}
+          sx={{
+            textAlign: { xs: 'center', md: 'left' },
+            mt: 2,
+            borderTop: '1px solid',
+            borderColor: palette.light.grey['300']
+          }}
         >
           <Grid item xs={12} md={6}>
-            <CopyRight textAlign='left'>
-              Pasar Protocol 2021 | Donate <span role="img" aria-label="">❤️</span> v1 - 6c57f5efda348a6c164b44e737a3abd4cdf03b6e
+            <CopyRight textAlign="left">
+              Pasar Protocol 2021 | Donate{' '}
+              <span role="img" aria-label="">
+                ❤️
+              </span>{' '}
+              v1 - 6c57f5efda348a6c164b44e737a3abd4cdf03b6e
             </CopyRight>
           </Grid>
           <Grid item xs={12} md={6}>
-            <CopyRight textAlign='right'>
-              Privacy Policy | Disclaimer️
-            </CopyRight>
+            <CopyRight textAlign="right">Privacy Policy | Disclaimer️</CopyRight>
           </Grid>
         </Grid>
       </Container>
