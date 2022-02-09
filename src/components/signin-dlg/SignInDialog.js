ff0c147e93382275b2739b73ca6f8f258b16c537
fullstack12345 <leo.hhu5731@gmail.com>
2022-02-09 17:33:40 +0900

    fix: qr code no popup after delete wallet session from essentials

--- src/components/signin-dlg/SignInDialog.js
+++ src/components/signin-dlg/SignInDialog.js
@@ -207,6 +207,7 @@ export default function SignInDialog() {
         setActivatingConnector(null);
         setWalletAddress(null);
         navigate('/marketplace');
+        window.location.reload();
       }
   }, [essentialsConnector.hasWalletConnectSession()]);
 
