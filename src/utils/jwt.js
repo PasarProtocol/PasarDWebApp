73343b8341fa64b7065c3d8633c6f50a5c9a849b
fullstack12345 <leo.hhu5731@gmail.com>
2022-02-11 10:24:32 +0900

    Fix issue in merging PR

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -31,13 +31,13 @@ const isValidToken = (accessToken) => {
 
 const setSession = (accessToken) => {
   if (accessToken) {
-    sessionStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('accessToken', accessToken);
     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
     // This function below will handle when token is expired
     // const { exp } = jwtDecode(accessToken);
     // handleTokenExpired(exp);
   } else {
-    sessionStorage.removeItem('accessToken');
+    localStorage.removeItem('accessToken');
     delete axios.defaults.headers.common.Authorization;
   }
 };
