23b0fe128c53873bba9e081a6df35a86c5f38b55
fullstack12345 <leo.hhu5731@gmail.com>
2022-03-01 19:09:49 +0900

    fix: show and close prompt instantly when hovering avatars

--- src/components/marketplace/BadgeProfile.js
+++ src/components/marketplace/BadgeProfile.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { isMobile } from 'react-device-detect';
 import { Link as RouterLink } from 'react-router-dom';
 // material
 import { useTheme, styled } from '@mui/material/styles';
-import { Box, Stack, Typography, Menu, Popover, Tooltip, Link } from '@mui/material';
+import { Box, Stack, Typography, Menu, Popover, Popper, Tooltip, Link } from '@mui/material';
+import Fade from '@mui/material/Fade';
 
 import Badge from '../Badge';
 import Jazzicon from '../Jazzicon';
@@ -11,7 +12,7 @@ import { reduceHexAddress, getDidInfoFromAddress } from '../../utils/common';
 import { stickerContract as STICKER_ADDRESS } from '../../config';
 // ----------------------------------------------------------------------
 
-const style = {
+const AvatarBoxStyle = {
   width: 70,
   height: 70,
   display: 'flex',
@@ -22,9 +23,11 @@ const style = {
     'linear-gradient(#fff, #fff) padding-box, linear-gradient(180deg, #a951f4, #FF5082) border-box',
   border: '2px solid transparent'
 }
+
 export default function BadgeProfile(props) {
   const {type, walletAddress, badge} = props
   const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = React.useState(false);
   const [didInfo, setDidInfo] = useState({name: '', description: ''});
 
   useEffect(()=>{
@@ -36,10 +39,11 @@ export default function BadgeProfile(props) {
   }, [walletAddress])
   const handlePopoverOpen = (event) => {
     if (isMobile && event.type === 'mouseenter') return;
-    setAnchorEl(event.currentTarget);
+    setAnchorEl(event.currentTarget)
+    setOpen(true);
   };
   const handlePopoverClose = () => {
-    setAnchorEl(null);
+    setOpen(false);
   };
 
   let title = "Feeds NFT Sticker"
@@ -50,12 +54,13 @@ export default function BadgeProfile(props) {
     dispAddress = didInfo.name?reduceHexAddress(walletAddress):''
     description = didInfo.description
   }
+
   return (
     <>
       <Box
         onClick={handlePopoverOpen}
         onMouseEnter={handlePopoverOpen}
-        // onMouseLeave={handlePopoverClose}
+        onMouseLeave={handlePopoverClose}
       >
         {
           type===1&&
@@ -68,8 +73,82 @@ export default function BadgeProfile(props) {
           <Jazzicon address={walletAddress} size={26} sx={{mr: 0}}/>
         }
       </Box>
-      <Menu
-        keepMounted
+      <Popper
+        open={open}
+        anchorEl={anchorEl}
+        onMouseEnter={()=>{setOpen(true)}}
+        onMouseLeave={()=>{setOpen(false)}}
+        placement="bottom-start"
+        transition
+      >
+        {
+          ({ TransitionProps }) => (
+            <Fade {...TransitionProps}>
+              <Stack sx={{minWidth: 180, p: 2, alignItems: 'center', borderRadius: 1, boxShadow: (theme) => theme.customShadows.z12, background: (theme) => theme.palette.background.paper}}>
+                <Box sx={{...AvatarBoxStyle}}>
+                  {
+                    type===1&&
+                    <Box sx={{ backgroundColor: 'black', borderRadius: '100%', width: 60, height: 60, display: 'flex', justifyContent: 'center' }}>
+                      <Box draggable = {false} component="img" src="/static/feeds-sticker.svg" sx={{ width: 35 }} />
+                    </Box>
+                  }
+                  {
+                    type===2&&
+                    <Link to={`/profile/others/${walletAddress}`} component={RouterLink} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <Jazzicon
+                        address={walletAddress}
+                        size={60}
+                        sx={{mr: 0}}
+                      />
+                    </Link>
+                  }
+                </Box>
+                {
+                  type===1?
+                  <Typography variant='h5' sx={{ pt: 2 }}>{title}</Typography>:
+
+                  <Link to={`/profile/others/${walletAddress}`} component={RouterLink}>
+                    <Typography variant='h5' sx={{ pt: 2 }}>{title}</Typography>
+                  </Link>
+                }
+                {
+                  dispAddress&&(
+                    type===1?
+                    <Typography variant='subtitle2' sx={{fontWeight: 'normal', fontSize: '0.925em'}}>{dispAddress}</Typography>:
+
+                    <Link to={`/profile/others/${walletAddress}`} component={RouterLink}>
+                      <Typography variant='subtitle2' sx={{fontWeight: 'normal', fontSize: '0.925em'}}>{dispAddress}</Typography>
+                    </Link>
+                  )
+                }
+                {
+                  description&&
+                  <Typography variant='subtitle2' sx={{fontWeight: 'normal', color: 'text.secondary', pt: 2, lineHeight: 1, fontSize: '0.925em'}}>{description}</Typography>
+                }
+                {
+                  type===2&&
+                  <Stack spacing={.5} direction="row" sx={{justifyContent: 'center', pt: (badge.dia||badge.kyc)?2:0}}>
+                    {
+                      badge.dia&&
+                      <Tooltip title="Diamond (DIA) token holder" arrow enterTouchDelay={0}>
+                        <Box><Badge name="diamond"/></Box>
+                      </Tooltip>
+                    }
+                    {
+                      badge.kyc&&
+                      <Tooltip title="KYC-ed user" arrow enterTouchDelay={0}>
+                        <Box><Badge name="user"/></Box>
+                      </Tooltip>
+                    }
+                  </Stack>
+                }
+              </Stack>
+            </Fade>
+          )
+        }
+      </Popper>
+      {/* <Menu
+        // keepMounted
         id="simple-menu"
         open={Boolean(anchorEl)}
         anchorEl={anchorEl}
@@ -77,67 +156,9 @@ export default function BadgeProfile(props) {
         transformOrigin={{ horizontal: 'left', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
         MenuListProps={{ onMouseLeave: handlePopoverClose }}
-      >
-        <Stack sx={{minWidth: 180, p: 2, alignItems: 'center'}}>
-          <Box sx={{...style}}>
-            {
-              type===1&&
-              <Box sx={{ backgroundColor: 'black', borderRadius: '100%', width: 60, height: 60, display: 'flex', justifyContent: 'center' }}>
-                <Box draggable = {false} component="img" src="/static/feeds-sticker.svg" sx={{ width: 35 }} />
-              </Box>
-            }
-            {
-              type===2&&
-              <Link to={`/profile/others/${walletAddress}`} component={RouterLink} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Jazzicon
-                  address={walletAddress}
-                  size={60}
-                  sx={{mr: 0}}
-                />
-              </Link>
-            }
-          </Box>
-          {
-            type===1?
-            <Typography variant='h5' sx={{ pt: 2 }}>{title}</Typography>:
-
-            <Link to={`/profile/others/${walletAddress}`} component={RouterLink}>
-              <Typography variant='h5' sx={{ pt: 2 }}>{title}</Typography>
-            </Link>
-          }
-          {
-            dispAddress&&(
-              type===1?
-              <Typography variant='subtitle2' sx={{fontWeight: 'normal', fontSize: '0.925em'}}>{dispAddress}</Typography>:
-
-              <Link to={`/profile/others/${walletAddress}`} component={RouterLink}>
-                <Typography variant='subtitle2' sx={{fontWeight: 'normal', fontSize: '0.925em'}}>{dispAddress}</Typography>
-              </Link>
-            )
-          }
-          {
-            description&&
-            <Typography variant='subtitle2' sx={{fontWeight: 'normal', color: 'text.secondary', pt: 2, lineHeight: 1, fontSize: '0.925em'}}>{description}</Typography>
-          }
-          {
-            type===2&&
-            <Stack spacing={.5} direction="row" sx={{justifyContent: 'center', pt: (badge.dia||badge.kyc)?2:0}}>
-              {
-                badge.dia&&
-                <Tooltip title="Diamond (DIA) token holder" arrow enterTouchDelay={0}>
-                  <Box><Badge name="diamond"/></Box>
-                </Tooltip>
-              }
-              {
-                badge.kyc&&
-                <Tooltip title="KYC-ed user" arrow enterTouchDelay={0}>
-                  <Box><Badge name="user"/></Box>
-                </Tooltip>
-              }
-            </Stack>
-          }
-        </Stack>
-      </Menu>
+        hideBackdrop={Boolean(true)}
+      >123
+      </Menu> */}
       {/* <Popover
         id="mouse-over-popover"
         sx={{
