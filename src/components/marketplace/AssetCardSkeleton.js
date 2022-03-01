41ba5fcb05f5d5c753343a8ebb85e7fbf29681b4
fullstack12345 <leo.hhu5731@gmail.com>
2022-03-01 13:56:11 +0900

    Revise card layout & skeleton card layout update badge profile component

--- src/components/marketplace/AssetCardSkeleton.js
+++ src/components/marketplace/AssetCardSkeleton.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import PaperRecord from '../PaperRecord';
 
 // ----------------------------------------------------------------------
 
 export default function AssetCardSkeleton() {
   return (
-      <PaperRecord sx={{p:2, mb: '2px'}}>
-        <SkeletonTheme borderRadius="0.5rem">
-          <Skeleton circle width={24} height={24} style={{marginBottom: 5}}/>
-          <Skeleton style={{ justifyContent: 'center', aspectRatio: '1/1', display: 'flex', marginBottom: -20 }} />
-          <h2><Skeleton/></h2>
-          <h5><Skeleton count={1}/></h5>
-          <h2><Skeleton/></h2>
-          <Box sx={{height: '20px'}}/>
-        </SkeletonTheme>
+      <PaperRecord>
+        {/* <SkeletonTheme borderRadius="0.5rem"> */}
+          <Stack direction="row" spacing={.5} sx={{p:2, pb: 1}}>
+            <Skeleton circle width={26} height={26}/>
+            <Skeleton circle width={26} height={26}/>
+          </Stack>
+          <Skeleton style={{ justifyContent: 'center', aspectRatio: '1/1' }} />
+          <Box sx={{p:2}}>
+            <h3><Skeleton/></h3>
+            <h3><Skeleton/></h3>
+          </Box>
+        {/* </SkeletonTheme> */}
       </PaperRecord>
   )
 }
