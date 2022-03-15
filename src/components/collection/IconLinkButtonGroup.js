86b99d457697f66e21e65a2160ff04002a9db22d
fullstack12345 <leo.hhu5731@gmail.com>
2022-03-15 19:44:36 +0900

    fix style of profile page according to dark mode

--- src/components/collection/IconLinkButtonGroup.js
+++ src/components/collection/IconLinkButtonGroup.js
@@ -10,29 +10,32 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import languageIcon from '@iconify/icons-ic/language';
 import discordIcon from '@iconify/icons-ic/sharp-discord'
 
+import useSettings from '../../hooks/useSettings';
 import { MFab } from '../@material-extend';
 import Discord from '../LinkIcons/Discord'
 // ----------------------------------------------------------------------
 export default function IconLinkButtonGroup(props) {
+  const { themeMode } = useSettings();
+  const colorType = themeMode==='light'?'primary':'default'
   return (
     <Stack
       spacing={1}
       direction="row"
       justifyContent='center'
     >
-      <MFab size="small">
+      <MFab size="small" color= {colorType}>
         <Icon icon={languageIcon} width={24} height={24} />
       </MFab>
-      <MFab size="small">
+      <MFab size="small" color= {colorType}>
         <Icon icon={twitterIcon} width={24} height={24} />
       </MFab>
-      <MFab size="small">
+      <MFab size="small" color= {colorType}>
         <Icon icon={discordIcon} width={24} height={24} />
       </MFab>
-      <MFab size="small">
+      <MFab size="small" color= {colorType}>
         <TelegramIcon />
       </MFab>
-      <MFab size="small">
+      <MFab size="small" color= {colorType}>
         <Icon icon={mediumIcon} width={24} height={24} />
       </MFab>
     </Stack>
