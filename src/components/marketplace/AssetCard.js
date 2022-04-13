3f21381eeee922959a337f8478008f8913383dcb
fullstack12345 <leo.hhu5731@gmail.com>
2022-04-13 12:00:33 +0900

    correct checking reserve price met or not

--- src/components/marketplace/AssetCard.js
+++ src/components/marketplace/AssetCard.js
@@ -218,7 +218,7 @@ export default function AssetCard(props) {
               {
                 holder&&<BadgeProfile type={2} walletAddress={holder} badge={badge}/>
               }
-              {!!(reservePrice*1)&&<BadgeProfile type={3} reservePriceFlag={currentBidPrice>=reservePrice}/>}
+              {!!(reservePrice*1)&&<BadgeProfile type={3} reservePriceFlag={currentBidPrice/1e18 >= reservePrice/1e18}/>}
               {!!(buyoutPrice*1)&&<BadgeProfile type={4}/>}
             </Stack>
             <Box>
