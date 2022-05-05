40b3797fcff4a2ec728d613758651787eea429d0
fullstack12345 <leo.hhu5731@gmail.com>
2022-05-05 10:10:08 +0900

    Make description input multi line input when creating an item

--- src/pages/marketplace/CreateItem.js
+++ src/pages/marketplace/CreateItem.js
@@ -1090,6 +1090,7 @@ export default function CreateItem() {
                     onChange={(e)=>setDescription(e.target.value)}
                     aria-describedby="description-error-text"
                     sx={{mt: '-5px !important'}}
+                    multiline={Boolean(true)}
                   />
                   <FormHelperText id="description-error-text" hidden={!isOnValidation||(isOnValidation&&description.length>0)}>Description is required</FormHelperText>
                 </FormControl>
