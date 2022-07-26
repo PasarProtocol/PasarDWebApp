76586eebe6d551f498c5b2aedb95cd05bc771c18
fullstack12345 <leo.hhu5731@gmail.com>
2022-07-26 10:00:11 +0900

    implement displaying video token in collectible card

--- src/components/CardImgBox.js
+++ src/components/CardImgBox.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Box, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { makeStyles } from "@mui/styles";
+import HoverVideoPlayer from "react-hover-video-player";
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { Icon } from '@iconify/react';
+
 import PaperRecord from './PaperRecord';
 import { getAssetImage, chainTypes } from '../utils/common'
 import useSettings from '../hooks/useSettings';
@@ -40,12 +44,67 @@ const BoxStyle = styled(Box)(({ theme }) => ({
     }
 }));
 
+const useStyles = makeStyles(theme => ({
+  spinnerBox: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spinner: {
+    width: '4em',
+    height: '4em',
+    border: '6px solid white',
+    borderRadius: '50%',
+    borderColor: 'white white transparent transparent',
+    animation: `$loadingOverlaySpinner 1000ms linear infinite`,
+  },
+  "@keyframes loadingOverlaySpinner": {
+    "0%": {
+      transform: 'rotate(0deg)'
+    },
+    "100%": {
+      transform: 'rotate(360deg)'
+    }
+  }
+}));
+const LoadingOverlay = ()=>{
+  const classes = useStyles();
+  return <Box className={classes.spinnerBox}>
+    <Box className={classes.spinner}/>
+  </Box>
+}
 const CardImgBox = (props) => {
     const { isMoreLink=false, isLink, thumbnail, marketPlace=1 } = props;
     const src = isLink? getAssetImage(props, true): props.thumbnail
     const imageRef = React.useRef();
+    const [isVideo, setIsVideo] = React.useState(false)
+    const [videoIsLoaded, setVideoIsLoaded] = React.useState(false)
     const [isAfterLoad, setIsAfterLoad] = React.useState(false)
 
+    React.useEffect(()=>{
+      fetch(src)
+        .then(response => {
+          const contentype = response.headers.get("content-type")
+          if(contentype.startsWith('video')) {
+            setIsVideo(true)
+          }
+        })
+        .catch(e=>{console.log(e)})
+    }, [])
+
+    React.useEffect(()=>{
+      if(isVideo) {
+        const obj = document.getElementById('videoId');
+        obj.addEventListener('loadeddata', () => {
+          if(obj.readyState >= 2) {
+            setVideoIsLoaded(true)
+          }
+        });
+      }
+    }, [isVideo])
     const handleResize = ()=>{
       setIsAfterLoad(true)
       // if(!imageRef.current)
@@ -85,26 +144,42 @@ const CardImgBox = (props) => {
     return (
       <BoxStyle className='card-img' sx={{opacity: isMoreLink?.5:1}}>
         {
-          !isAfterLoad &&
+          ((!isVideo && !isAfterLoad) || (isVideo && !videoIsLoaded)) &&
           <SkeletonTheme {...themeProp}>
             <Skeleton style={{ justifyContent: 'center', aspectRatio: '1/1', padding: 2, zIndex: 1}} />
           </SkeletonTheme>
         }
         <Box className='img-box' ref={imageRef}>
-          <LazyLoadImage 
-            src={src}
-            effect="blur" 
-            wrapperProps={{
-              style:{
-                display: 'contents'
-                // width: isAfterLoad?'auto':0,
-                // display: 'inline-flex'
-              }
-            }} 
-            style={{...imageStyle}} 
-            afterLoad={handleResize} 
-            onError={handleErrorImage}
-          />
+          {
+            !isVideo?
+            <LazyLoadImage 
+              src={src}
+              effect="blur" 
+              wrapperProps={{
+                style:{
+                  display: 'contents'
+                  // width: isAfterLoad?'auto':0,
+                  // display: 'inline-flex'
+                }
+              }} 
+              style={{...imageStyle}} 
+              afterLoad={handleResize} 
+              onError={handleErrorImage}
+            />:
+
+            <>
+              <HoverVideoPlayer
+                videoSrc={src}
+                // pausedOverlay={<PausedOverlay />}
+                loadingOverlay={<LoadingOverlay />}
+                videoId="videoId"
+                style={{width: '100%', height: '100%'}}
+              />
+              <Box sx={{position: 'absolute', top: 10, right: 10, color: '#eee', fontSize: '20pt', display: 'inline-block', lineHeight: 0, borderRadius: '50%', background: 'black'}}>
+                <Icon icon="fa6-regular:circle-play" />
+              </Box>
+            </>
+          }
         </Box>
         <Box className='chain-type' sx={{position: 'absolute', bottom: 10, width: '100%', textAlign: 'center', opacity: 0, transition: 'opacity .2s'}}>
           <Box sx={{bgcolor: chainTypes[marketPlace-1].color, borderRadius: 2, display: 'inline-flex', px: '10px', py: 1}}>
