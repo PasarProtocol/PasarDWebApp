324f82009f67c0488590bc880e99455aa1fa3791
fullstack12345 <leo.hhu5731@gmail.com>
2022-07-27 09:01:30 +0900

    Update Volume emoji and put logo next to the numbers (volume and floor) for all networks Add Fusion logo on Trading volume and floor price remove needless images

--- src/components/collection/StatisticPanel/StatisticItem.js
+++ src/components/collection/StatisticPanel/StatisticItem.js
@@ -5,6 +5,7 @@ import AnimatedNumber from 'react-animated-number';
 import { styled } from '@mui/material/styles';
 import { Typography, Tooltip, Box, Stack } from '@mui/material';
 
+import VolumeIcon from '../VolumeIcon';
 // ----------------------------------------------------------------------
 const tooltipText = ['Total trading volume', 'Amount of minted items', 'Cheapest item on sale', 'Unique number of owners']
 
@@ -24,24 +25,6 @@ const RootStyle = styled('div')(({ theme, index }) => {
     [theme.breakpoints.down('sm')]: sm
   }
 });
-const VolumeIcon = (props)=>{
-  const { marketPlace } = props
-  const volumeIconTypes = [
-    {icon: 'elastos.svg', style: { filter: (theme)=>theme.palette.mode==='dark'?'invert(1)':'none' }},
-    {icon: 'ethereum.svg', style: { filter: (theme)=>theme.palette.mode==='light'?'invert(0.8)':'invert(0.2)', width: 16 }}
-  ]
-  let volumeIcon = null
-  if(marketPlace>0)
-    volumeIcon = volumeIconTypes[marketPlace-1]
-  else if(marketPlace===undefined)
-    volumeIcon = null
-  else
-    [volumeIcon] = volumeIconTypes
-
-  return volumeIcon?
-        <Box component="img" src={`/static/${volumeIcon.icon}`} sx={{ width: 18, display: 'inline', verticalAlign: 'middle', ...volumeIcon.style }} />:
-        null
-}
 
 export default function StatisticItem(props) {
   const { index, value, marketPlace } = props
