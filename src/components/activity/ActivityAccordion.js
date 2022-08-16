f79be0e98ba3a0773aec6baae6d9a3cb76e02740
fullstack12345 <leo.hhu5731@gmail.com>
2022-08-16 18:27:28 +0900

    add: link to collectible, collection, profile page when click field of activity on mobile view

--- src/components/activity/ActivityAccordion.js
+++ src/components/activity/ActivityAccordion.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import * as math from 'mathjs';
 import 'react-loading-skeleton/dist/skeleton.css'
 import { Box, Stack, Accordion, AccordionSummary, AccordionDetails, Typography, Link, Divider } from '@mui/material';
@@ -6,6 +7,7 @@ import { Box, Stack, Accordion, AccordionSummary, AccordionDetails, Typography, 
 import TabletImgBox from './TabletImgBox'
 import KYCBadge from '../badge/KYCBadge';
 import { getCoinTypeFromToken, getDateDistance, reduceHexAddress } from '../../utils/common';
+import { blankAddress } from '../../config';
 // ----------------------------------------------------------------------
 
 const ActivityAccordion = (props) => {
@@ -20,9 +22,16 @@ const ActivityAccordion = (props) => {
     const dispAddress = infoByAddress[addrstr] ? infoByAddress[addrstr].name : reduceHexAddress(addrstr)
     return (
       <Stack direction="row" spacing={1} alignItems="center" display="inline-flex">
-        <Typography variant="body2" color="origin.main" display="inline-flex">
-          {dispAddress || "---"}
-        </Typography>
+        {
+          dispAddress.length>0 && addrstr!==blankAddress?
+          <Link to={`/profile/others/${addrstr}`} component={RouterLink} color='inherit'>
+            <Typography variant="body2" color="origin.main" display="inline-flex">
+              {dispAddress}
+            </Typography>
+          </Link>:
+
+          <Typography variant="body2" color="origin.main" display="inline-flex">{dispAddress || '---'}</Typography>
+        }
         {
           infoByAddress[addrstr] && infoByAddress[addrstr].kyc && <KYCBadge/>
         }
@@ -37,26 +46,35 @@ const ActivityAccordion = (props) => {
       sx={{
         border: '1px solid',
         borderColor: 'action.disabledBackground',
-        boxShadow: (theme) => theme.customShadows.z1
+        boxShadow: (theme) => theme.customShadows.z1,
+        cursor: 'non'
       }}
     >
       <AccordionSummary sx={{ '& .MuiAccordionSummary-content': {width: "100%"} }}>
         <Stack direction="row" spacing={1} width="100%" alignItems="center">
-          <Box sx={{width: 50, height: 50}}>
-            <TabletImgBox {...trans}/>
-          </Box>
+          <Link to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink} sx={{ color: 'inherit' }}>
+            <Box sx={{width: 50, height: 50}}>
+              <TabletImgBox {...trans}/>
+            </Box>
+          </Link>
           <Stack flex={1} minWidth={0}>
             <Stack direction="row">
               <Box sx={{ minWidth: 0, flexGrow: 1 }}>
-                <Typography variant="body2" color="text.secondary" noWrap>{trans.collectionName}</Typography>
+                <Link to={`/collections/detail/${trans.marketPlace}${trans.baseToken}`} component={RouterLink} sx={{ color: 'inherit', display: 'inline-flex', maxWidth: 'max-content', width: '100%' }}>
+                  <Typography variant="body2" color="text.secondary" noWrap>{trans.collectionName}</Typography>
+                </Link>
               </Box>
               <Box>
-                <Typography variant="body2" color="text.secondary" display="inline">{ trans.type }</Typography>
+                <Link to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink}>
+                  <Typography variant="body2" color="text.secondary" display="inline">{ trans.type }</Typography>
+                </Link>
               </Box>
             </Stack>
             <Stack direction="row">
               <Box sx={{ minWidth: 0, flexGrow: 1 }}>
-                <Typography variant="subtitle2" noWrap>{trans.name}</Typography>
+                <Link to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink} sx={{ color: 'inherit', display: 'inline-flex', maxWidth: 'max-content', width: '100%' }}>
+                  <Typography variant="subtitle2" noWrap>{trans.name}</Typography>
+                </Link>
               </Box>
               <Box component="img" src={`/static/${coinType.icon}`} sx={{ width: 16, display: 'inline', filter: (theme)=>theme.palette.mode==='dark'&&coinType.index===0?'invert(1)':'none' }} />
               <Typography variant="subtitle2" color="origin.main" textAlign="left" display='inline-flex' ml={1}>{priceVal}</Typography>
