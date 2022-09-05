bc28932548de0657177966d3b5d940405f3a4bb8
blockchaindev91 <hisamitsuteru@gmail.com>
2022-09-05 17:04:13 +0900

    fix: update statisticPanel component

--- src/components/rewards/StatisticPanel/index.js
+++ src/components/rewards/StatisticPanel/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 // material
 import { styled } from '@mui/material/styles';
 import { Grid, Card, Box } from '@mui/material';
@@ -15,21 +16,28 @@ const RootStyle = styled(Card)(({ theme }) => ({
   }
 }));
 
-export default function StatisticPanel() {
+StatisticPanel.propTypes = {
+  itemCount: PropTypes.number,
+  poolRatio: PropTypes.number,
+  userCount: PropTypes.number,
+  nextDistribution: PropTypes.number
+}
+
+export default function StatisticPanel({ itemCount, poolRatio, userCount, nextDistribution }) {
   return (
     <RootStyle>
       <Grid container>
         <Grid item xs={6} sm={3} md={3}>
-          <StatisticItem title="🛒 Items Listed" index={1}/>
+          <StatisticItem title="🛒 Items Listed" index={1} data={itemCount} />
         </Grid>
         <Grid item xs={6} sm={3} md={3}>
-          <StatisticItem title="🏊 Pool Ratio" index={2}/>
+          <StatisticItem title="🏊 Pool Ratio" index={2} data={poolRatio} />
         </Grid>
         <Grid item xs={6} sm={3} md={3}>
-          <StatisticItem title="💰 Users Rewarded" index={3}/>
+          <StatisticItem title="💰 Users Rewarded" index={3} data={userCount} />
         </Grid>
         <Grid item xs={6} sm={3} md={3}>
-          <StatisticItem title="🧱 Next Distribution" index={4}/>
+          <StatisticItem title="🧱 Next Distribution" index={4} data={nextDistribution} />
         </Grid>
       </Grid>
     </RootStyle>
