875795d171a0315470c420b09452a6df75426dad
blockchaindev91 <hisamitsuteru@gmail.com>
2022-10-12 11:25:42 +0900

    fix: update routes

--- src/App.js
+++ src/App.js
@@ -25,37 +25,40 @@ export default function App() {
     // Prevent F12 -
     if (event.keyCode === 123) {
       event.preventDefault();
-      return;  
+      return;
     }
     // Prevent Ctrl+u = disable view page source
     // Prevent Ctrl+s = disable save
     // Prevent Ctrl+a = disable select all
     // Prevent Ctrl+d = disable bookmark
-    if (event.ctrlKey && (event.keyCode === 85 || event.keyCode === 83 || event.keyCode ===65 || event.keyCode ===68 )){
+    if (
+      event.ctrlKey &&
+      (event.keyCode === 85 || event.keyCode === 83 || event.keyCode === 65 || event.keyCode === 68)
+    ) {
       event.preventDefault();
       return;
     }
     // Prevent Ctrl+Shift+I = disabled debugger console using keys open
-    if (event.ctrlKey && event.shiftKey && event.keyCode === 73){
+    if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
       event.preventDefault();
     }
   });
-  
-  document.addEventListener("wheel", () => {  
-    if (document.activeElement.type === "number") {  
-      document.activeElement.blur();  
-    }  
+
+  document.addEventListener('wheel', () => {
+    if (document.activeElement.type === 'number') {
+      document.activeElement.blur();
+    }
   });
-  
+
   return (
     <ThemeConfig>
       <ThemePrimaryColor>
         <ThemeLocalization>
-            <NotistackProvider>
-              <GlobalStyles />
-              <ProgressBarStyle />
-              <Router />
-            </NotistackProvider>
+          <NotistackProvider>
+            <GlobalStyles />
+            <ProgressBarStyle />
+            <Router />
+          </NotistackProvider>
         </ThemeLocalization>
       </ThemePrimaryColor>
     </ThemeConfig>
