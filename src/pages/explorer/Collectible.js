ef33b47a82a981748f0c95b473e65168ebe6bb33
blockchaindev91 <hisamitsuteru@gmail.com>
2022-10-13 19:25:45 +0900

    fix: clean collectible page

--- src/pages/explorer/Collectible.js
+++ src/pages/explorer/Collectible.js
@@ -1,13 +1,13 @@
 // material
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
-import { Container, Stack, Grid, Paper, Typography, Link } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Container, Stack, Grid, Typography } from '@mui/material';
 // components
 import Page from '../../components/Page';
 import LoadingScreen from '../../components/LoadingScreen';
 import Scrollbar from '../../components/Scrollbar';
-import CollectibleListItem from '../../components/explorer/CollectibleList/CollectibleListItem'
+import CollectibleListItem from '../../components/explorer/CollectibleList/CollectibleListItem';
 import ShowSelect from '../../components/pagination/ShowSelect';
 import Pagination from '../../components/pagination';
 import PaperRecord from '../../components/PaperRecord';
@@ -30,7 +30,7 @@ const RootStyle = styled(Page)(({ theme }) => ({
 const StackStyle = styled(Stack)(({ theme }) => ({
   flexDirection: 'row',
   [theme.breakpoints.down('md')]: {
-    flexDirection: 'column',
+    flexDirection: 'column'
   }
 }));
 // ----------------------------------------------------------------------
@@ -45,76 +45,87 @@ export default function Collectible() {
   const [timeOrder, setTimeOrder] = React.useState(-1);
   const [controller, setAbortController] = React.useState(new AbortController());
   const [isLoadingCollectibles, setLoadingCollectibles] = React.useState(false);
-  React.useEffect(async () => {
-    controller.abort(); // cancel the previous request
-    const newController = new AbortController();
-    const {signal} = newController;
-    setAbortController(newController);
-
-    setLoadingCollectibles(true);
-    fetchFrom(`api/v2/sticker/listStickers?pageNum=${page}&pageSize=${showCount}&timeOrder=${timeOrder}`, { signal }).then(response => {
-      response.json().then(jsonCollectibles => {
-        setTotalCount(jsonCollectibles.data.total)
-        setPages(Math.ceil(jsonCollectibles.data.total/showCount));
-        setCollectibles(jsonCollectibles.data.result);
-        setLoadingCollectibles(false);
-      })
-    }).catch(e => {
-      if(e.code !== e.ABORT_ERR)
-        setLoadingCollectibles(false);
-    });
-  }, [page, showCount, timeOrder]);
   
+  React.useEffect(() => {
+    const fetchData = async () => {
+      controller.abort(); // cancel the previous request
+      const newController = new AbortController();
+      const { signal } = newController;
+      setAbortController(newController);
+
+      setLoadingCollectibles(true);
+      fetchFrom(`api/v2/sticker/listStickers?pageNum=${page}&pageSize=${showCount}&timeOrder=${timeOrder}`, { signal })
+        .then((response) => {
+          response.json().then((jsonCollectibles) => {
+            setTotalCount(jsonCollectibles.data.total);
+            setPages(Math.ceil(jsonCollectibles.data.total / showCount));
+            setCollectibles(jsonCollectibles.data.result);
+            setLoadingCollectibles(false);
+          });
+        })
+        .catch((e) => {
+          if (e.code !== e.ABORT_ERR) setLoadingCollectibles(false);
+        });
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, showCount, timeOrder]);
+
   const changeShowCount = (event) => {
-    setShowCount(event.target.value)
+    setShowCount(event.target.value);
   };
-  const handleDateOrder = (selected)=>{
-    setTimeOrder(selected)
-  }
-  const link2Detail = (tokenId, baseToken)=>{
+  const handleDateOrder = (selected) => {
+    setTimeOrder(selected);
+  };
+  const link2Detail = (tokenId, baseToken) => {
     navigate(`/explorer/collectible/detail/${[tokenId, baseToken].join('&')}`);
-  }
+  };
   return (
     <RootStyle title="Collectible | PASAR">
       <Container maxWidth="lg">
         <StackStyle sx={{ mb: 2 }}>
-            <Typography variant="h4" sx={{flex:1}}>
-                All Collectibles
-                <DateOrderSelect onChange={handleDateOrder}/>
-                <Typography variant="body2" sx={{ display: 'inline-block' }}>{totalCount.toLocaleString('en')} items</Typography>
+          <Typography variant="h4" sx={{ flex: 1 }}>
+            All Collectibles
+            <DateOrderSelect onChange={handleDateOrder} />
+            <Typography variant="body2" sx={{ display: 'inline-block' }}>
+              {totalCount.toLocaleString('en')} items
             </Typography>
-            <Pagination page={page} pages={pages} onChange={setPage} />
+          </Typography>
+          <Pagination page={page} pages={pages} onChange={setPage} />
         </StackStyle>
-        {isLoadingCollectibles && <LoadingWrapper><LoadingScreen sx={{background: 'transparent'}}/></LoadingWrapper>}
+        {isLoadingCollectibles && (
+          <LoadingWrapper>
+            <LoadingScreen sx={{ background: 'transparent' }} />
+          </LoadingWrapper>
+        )}
         <Scrollbar>
-            <Grid container spacing={2}>
+          <Grid container spacing={2}>
             {collectibles.map((item, key) => (
-                <Grid key={key} item xs={12}>
-                    <PaperRecord sx={{
-                        textAlign: 'center',
-                        cursor: 'pointer'
-                      }}
-                      onClick={()=>link2Detail(item.tokenId, item.baseToken)}
-                    >
-                        <CollectibleListItem
-                            item={item}
-                        />
-                    </PaperRecord>
-                </Grid>
+              <Grid key={key} item xs={12}>
+                <PaperRecord
+                  sx={{
+                    textAlign: 'center',
+                    cursor: 'pointer'
+                  }}
+                  onClick={() => link2Detail(item.tokenId, item.baseToken)}
+                >
+                  <CollectibleListItem item={item} />
+                </PaperRecord>
+              </Grid>
             ))}
-            </Grid>
+          </Grid>
         </Scrollbar>
-        {
-            collectibles.length>0 &&
-            <StackStyle  sx={{ mt: 2, display: 'block' }}>
-                <Pagination page={page} pages={pages} onChange={setPage}/>
-                <ShowSelect count={showCount} onChange={changeShowCount}/>
-            </StackStyle>
-        }
-        {
-            !isLoadingCollectibles && !collectibles.length &&
-            <Typography variant="h4" align='center'>No collectible found!</Typography>
-        }
+        {collectibles.length > 0 && (
+          <StackStyle sx={{ mt: 2, display: 'block' }}>
+            <Pagination page={page} pages={pages} onChange={setPage} />
+            <ShowSelect count={showCount} onChange={changeShowCount} />
+          </StackStyle>
+        )}
+        {!isLoadingCollectibles && !collectibles.length && (
+          <Typography variant="h4" align="center">
+            No collectible found!
+          </Typography>
+        )}
       </Container>
     </RootStyle>
   );
