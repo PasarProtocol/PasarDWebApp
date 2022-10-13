674373b6e8ada959b3f107fd4d2f6254654f438f
blockchaindev91 <hisamitsuteru@gmail.com>
2022-10-13 18:31:32 +0900

    fix: update routes

--- src/routes/paths.js
+++ src/routes/paths.js
@@ -1,24 +1,3 @@
-// ----------------------------------------------------------------------
-
-function path(root, sublink) {
-  return `${root}${sublink}`;
-}
-
-const ROOTS_AUTH = '/auth';
-const ROOTS_DASHBOARD = '/dashboard';
-
-// ----------------------------------------------------------------------
-
-export const PATH_AUTH = {
-  root: ROOTS_AUTH,
-  login: path(ROOTS_AUTH, '/login'),
-  loginUnprotected: path(ROOTS_AUTH, '/login-unprotected'),
-  register: path(ROOTS_AUTH, '/register'),
-  registerUnprotected: path(ROOTS_AUTH, '/register-unprotected'),
-  resetPassword: path(ROOTS_AUTH, '/reset-password'),
-  verify: path(ROOTS_AUTH, '/verify')
-};
-
 export const PATH_PAGE = {
   explorer: '/explorer',
   marketplace: '/marketplace',
