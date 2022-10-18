86be1a840bcd7ba448e63ed9cfe66f12bbd36e12
blockchaindev91 <hisamitsuteru@gmail.com>
2022-10-18 17:28:12 +0900

    fix: fix console warnings

--- src/components/activity/ActivityAccordion.js
+++ src/components/activity/ActivityAccordion.js
@@ -1,46 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 import * as math from 'mathjs';
-import 'react-loading-skeleton/dist/skeleton.css'
+import 'react-loading-skeleton/dist/skeleton.css';
 import { Box, Stack, Accordion, AccordionSummary, AccordionDetails, Typography, Link, Divider } from '@mui/material';
 
-import TabletImgBox from './TabletImgBox'
+import TabletImgBox from './TabletImgBox';
 import KYCBadge from '../badge/KYCBadge';
 import { getCoinTypeFromToken, getDateDistance, reduceHexAddress } from '../../utils/common';
 import { blankAddress } from '../../config';
 // ----------------------------------------------------------------------
+ActivityAccordion.propTypes = {
+  trans: PropTypes.any,
+  coinPrice: PropTypes.any,
+  infoByAddress: PropTypes.any
+};
 
 const ActivityAccordion = (props) => {
-  const {trans, coinPrice, infoByAddress} = props
-  const [isMoreOpen, setMoreOpen] = React.useState(false)
-  const priceVal = trans.price ? math.round(trans.price/1e18, 3) : 0
-  const coinType = getCoinTypeFromToken(trans)
-  const coinUSD = coinPrice[coinType.index]
+  const { trans, coinPrice, infoByAddress } = props;
+  const [isMoreOpen, setMoreOpen] = React.useState(false);
+  const priceVal = trans.price ? math.round(trans.price / 1e18, 3) : 0;
+  const coinType = getCoinTypeFromToken(trans);
+  const coinUSD = coinPrice[coinType.index];
 
-  const AddressCell = ({field}) => {
-    const addrstr = trans[field]
-    const dispAddress = infoByAddress[addrstr] ? infoByAddress[addrstr].name : reduceHexAddress(addrstr)
+  AddressCell.propTypes = {
+    field: PropTypes.any
+  };
+  const AddressCell = ({ field }) => {
+    const addrstr = trans[field];
+    const dispAddress = infoByAddress[addrstr] ? infoByAddress[addrstr].name : reduceHexAddress(addrstr);
     return (
       <Stack direction="row" spacing={1} alignItems="center" display="inline-flex">
-        {
-          dispAddress.length>0 && addrstr!==blankAddress?
-          <Link to={`/profile/others/${addrstr}`} component={RouterLink} color='inherit'>
+        {dispAddress.length > 0 && addrstr !== blankAddress ? (
+          <Link to={`/profile/others/${addrstr}`} component={RouterLink} color="inherit">
             <Typography variant="body2" color="origin.main" display="inline-flex">
               {dispAddress}
             </Typography>
-          </Link>:
-
-          <Typography variant="body2" color="origin.main" display="inline-flex">---</Typography>
-        }
-        {
-          infoByAddress[addrstr] && infoByAddress[addrstr].kyc && <KYCBadge/>
-        }
+          </Link>
+        ) : (
+          <Typography variant="body2" color="origin.main" display="inline-flex">
+            ---
+          </Typography>
+        )}
+        {infoByAddress[addrstr] && infoByAddress[addrstr].kyc && <KYCBadge />}
       </Stack>
-    )
-  }
+    );
+  };
 
   return (
-    <Accordion 
+    <Accordion
       expanded={isMoreOpen}
       // onClick={(e) => handleAccordClick(key)}
       sx={{
@@ -50,64 +58,112 @@ const ActivityAccordion = (props) => {
         cursor: 'non'
       }}
     >
-      <AccordionSummary sx={{ '& .MuiAccordionSummary-content': {width: "100%"} }}>
+      <AccordionSummary sx={{ '& .MuiAccordionSummary-content': { width: '100%' } }}>
         <Stack direction="row" spacing={1} width="100%" alignItems="center">
-          <Link to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink} sx={{ color: 'inherit' }}>
-            <Box sx={{width: 50, height: 50}}>
-              <TabletImgBox {...trans}/>
+          <Link
+            to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+            component={RouterLink}
+            sx={{ color: 'inherit' }}
+          >
+            <Box sx={{ width: 50, height: 50 }}>
+              <TabletImgBox {...trans} />
             </Box>
           </Link>
           <Stack flex={1} minWidth={0}>
             <Stack direction="row">
               <Box sx={{ minWidth: 0, flexGrow: 1 }}>
-                <Link to={`/collections/detail/${trans.marketPlace}${trans.baseToken}`} component={RouterLink} sx={{ color: 'inherit', display: 'inline-flex', maxWidth: 'max-content', width: '100%' }}>
-                  <Typography variant="body2" color="text.secondary" noWrap>{trans.collectionName}</Typography>
+                <Link
+                  to={`/collections/detail/${trans.marketPlace}${trans.baseToken}`}
+                  component={RouterLink}
+                  sx={{ color: 'inherit', display: 'inline-flex', maxWidth: 'max-content', width: '100%' }}
+                >
+                  <Typography variant="body2" color="text.secondary" noWrap>
+                    {trans.collectionName}
+                  </Typography>
                 </Link>
               </Box>
               <Box>
-                <Link to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink}>
-                  <Typography variant="body2" color="text.secondary" display="inline">{ trans.type }</Typography>
+                <Link
+                  to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+                  component={RouterLink}
+                >
+                  <Typography variant="body2" color="text.secondary" display="inline">
+                    {trans.type}
+                  </Typography>
                 </Link>
               </Box>
             </Stack>
             <Stack direction="row">
               <Box sx={{ minWidth: 0, flexGrow: 1 }}>
-                <Link to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink} sx={{ color: 'inherit', display: 'inline-flex', maxWidth: 'max-content', width: '100%' }}>
-                  <Typography variant="subtitle2" noWrap>{trans.name}</Typography>
+                <Link
+                  to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+                  component={RouterLink}
+                  sx={{ color: 'inherit', display: 'inline-flex', maxWidth: 'max-content', width: '100%' }}
+                >
+                  <Typography variant="subtitle2" noWrap>
+                    {trans.name}
+                  </Typography>
                 </Link>
               </Box>
-              <Box component="img" src={`/static/${coinType.icon}`} sx={{ width: 16, display: 'inline', filter: (theme)=>theme.palette.mode==='dark'&&coinType.index===0?'invert(1)':'none' }} />
-              <Typography variant="subtitle2" color="origin.main" textAlign="left" display='inline-flex' ml={1}>{priceVal}</Typography>
+              <Box
+                component="img"
+                src={`/static/${coinType.icon}`}
+                sx={{
+                  width: 16,
+                  display: 'inline',
+                  filter: (theme) => (theme.palette.mode === 'dark' && coinType.index === 0 ? 'invert(1)' : 'none')
+                }}
+              />
+              <Typography variant="subtitle2" color="origin.main" textAlign="left" display="inline-flex" ml={1}>
+                {priceVal}
+              </Typography>
             </Stack>
             <Stack direction="row">
               <Box flexGrow={1}>
-                <Link color="text.secondary" onClick={()=>{setMoreOpen(!isMoreOpen)}}>
-                  <Typography variant="body2" noWrap display="inline-flex">{isMoreOpen?'-':'+'} more</Typography>
+                <Link
+                  color="text.secondary"
+                  onClick={() => {
+                    setMoreOpen(!isMoreOpen);
+                  }}
+                >
+                  <Typography variant="body2" noWrap display="inline-flex">
+                    {isMoreOpen ? '-' : '+'} more
+                  </Typography>
                 </Link>
               </Box>
-              <Typography variant="body2" color="text.secondary">{getDateDistance(trans.marketTime)}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                {getDateDistance(trans.marketTime)}
+              </Typography>
             </Stack>
           </Stack>
         </Stack>
       </AccordionSummary>
-      <AccordionDetails sx={{pt: 0}}>
-        <Divider/>
+      <AccordionDetails sx={{ pt: 0 }}>
+        <Divider />
         <Stack direction="row" textAlign="center" pt={1}>
           <Box flexGrow={1}>
-            <Typography variant="body2" color="text.secondary" noWrap>USD Price</Typography>
-            <Typography variant="body2" noWrap>${math.round(coinUSD * priceVal, 2)}</Typography>
+            <Typography variant="body2" color="text.secondary" noWrap>
+              USD Price
+            </Typography>
+            <Typography variant="body2" noWrap>
+              ${math.round(coinUSD * priceVal, 2)}
+            </Typography>
           </Box>
           <Box flexGrow={1}>
-            <Typography variant="body2" color="text.secondary" noWrap>From</Typography>
-            <AddressCell field="sellerAddr"/>
+            <Typography variant="body2" color="text.secondary" noWrap>
+              From
+            </Typography>
+            <AddressCell field="sellerAddr" />
           </Box>
           <Box flexGrow={1}>
-            <Typography variant="body2" color="text.secondary" noWrap>To</Typography>
-            <AddressCell field="buyerAddr"/>
+            <Typography variant="body2" color="text.secondary" noWrap>
+              To
+            </Typography>
+            <AddressCell field="buyerAddr" />
           </Box>
         </Stack>
       </AccordionDetails>
     </Accordion>
-  )
-}
-export default React.memo(ActivityAccordion)
+  );
+};
+export default React.memo(ActivityAccordion);
--- src/components/activity/ActivityTableRow.js
+++ src/components/activity/ActivityTableRow.js
@@ -1,106 +1,168 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 import * as math from 'mathjs';
-import 'react-loading-skeleton/dist/skeleton.css'
+import 'react-loading-skeleton/dist/skeleton.css';
 import { Box, Stack, Typography, TableRow, Link, TableCell } from '@mui/material';
 
-import TabletImgBox from './TabletImgBox'
+import TabletImgBox from './TabletImgBox';
 import KYCBadge from '../badge/KYCBadge';
 import { getCoinTypeFromToken, getDateDistance, reduceHexAddress, MethodList } from '../../utils/common';
 import { blankAddress } from '../../config';
 // ----------------------------------------------------------------------
 
 const EventByType = {
-  "Minted": "Mint",
-  "Listed": "CreateOrderForSale",
-  "Sale": "BuyOrder"
-}
+  Minted: 'Mint',
+  Listed: 'CreateOrderForSale',
+  Sale: 'BuyOrder'
+};
+
+ActivityTableRow.propTypes = {
+  trans: PropTypes.any,
+  coinPrice: PropTypes.any,
+  infoByAddress: PropTypes.any,
+  COLUMNS: PropTypes.any
+};
 
 const ActivityTableRow = (props) => {
-  const {trans, coinPrice, infoByAddress, COLUMNS} = props
+  const { trans, coinPrice, infoByAddress, COLUMNS } = props;
   return (
     <TableRow hover tabIndex={-1}>
       {COLUMNS.map((column) => {
-        let cellcontent = ''
-        switch(column.id) {
-          case "type": {
-            const originEvent = EventByType[trans.type]
-            let methodItem = MethodList.find((item)=>item.method===originEvent)
-            if(!methodItem)
-                methodItem = {color: 'grey', icon: 'tag', detail: []}
-            // const explorerSrvUrl = getExplorerSrvByNetwork(trans.marketPlace)
-            cellcontent = 
-              <Stack direction="row" alignItems="center" spacing={2}>
-                <Link to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink}>
-                  <Box
-                    component="img"
-                    alt=""
-                    src={`/static/${methodItem.icon}.svg`}
-                    sx={{ width: 50, height: 50, borderRadius: 1, cursor: 'pointer', background: methodItem.color, p: 2 }}
-                  />
-                </Link>
-                <Link to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink}>
-                  <Typography variant="subtitle2">{ trans.type }</Typography>
-                </Link>
-              </Stack>
-          }
+        let cellcontent = '';
+        switch (column.id) {
+          case 'type':
+            {
+              const originEvent = EventByType[trans.type];
+              let methodItem = MethodList.find((item) => item.method === originEvent);
+              if (!methodItem) methodItem = { color: 'grey', icon: 'tag', detail: [] };
+              cellcontent = (
+                <Stack direction="row" alignItems="center" spacing={2}>
+                  <Link
+                    to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+                    component={RouterLink}
+                  >
+                    <Box
+                      component="img"
+                      alt=""
+                      src={`/static/${methodItem.icon}.svg`}
+                      sx={{
+                        width: 50,
+                        height: 50,
+                        borderRadius: 1,
+                        cursor: 'pointer',
+                        background: methodItem.color,
+                        p: 2
+                      }}
+                    />
+                  </Link>
+                  <Link
+                    to={`/explorer/collectible/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+                    component={RouterLink}
+                  >
+                    <Typography variant="subtitle2">{trans.type}</Typography>
+                  </Link>
+                </Stack>
+              );
+            }
             break;
-          case "image":
-            cellcontent = 
+          case 'image':
+            cellcontent = (
               <Stack direction="row" spacing={2}>
-                <Link to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink} sx={{ color: 'inherit' }}>
-                  <Box sx={{width: 50, height: 50}}>
-                    <TabletImgBox {...trans}/>
+                <Link
+                  to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+                  component={RouterLink}
+                  sx={{ color: 'inherit' }}
+                >
+                  <Box sx={{ width: 50, height: 50 }}>
+                    <TabletImgBox {...trans} />
                   </Box>
                 </Link>
                 <Stack textAlign="left">
-                  <Link to={`/collections/detail/${trans.marketPlace}${trans.baseToken}`} component={RouterLink} sx={{ color: 'inherit' }}>
-                    <Typography variant="body2" color="text.secondary">{trans.collectionName}</Typography>
+                  <Link
+                    to={`/collections/detail/${trans.marketPlace}${trans.baseToken}`}
+                    component={RouterLink}
+                    sx={{ color: 'inherit' }}
+                  >
+                    <Typography variant="body2" color="text.secondary">
+                      {trans.collectionName}
+                    </Typography>
                   </Link>
-                  <Link to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`} component={RouterLink} sx={{ color: 'inherit' }}>
+                  <Link
+                    to={`/marketplace/detail/${[trans.tokenId, trans.baseToken].join('&')}`}
+                    component={RouterLink}
+                    sx={{ color: 'inherit' }}
+                  >
                     <Typography variant="subtitle2">{trans.name}</Typography>
                   </Link>
                 </Stack>
               </Stack>
+            );
             break;
-          case "price": {
-            const priceVal = trans.price ? math.round(trans.price/1e18, 3) : 0
-            const coinType = getCoinTypeFromToken(trans)
-            const coinUSD = coinPrice[coinType.index]
-            cellcontent = 
-              <Stack display="inline-flex" textAlign="left">
-                <Stack direction='row' spacing={1}>
-                  <Box component="img" src={`/static/${coinType.icon}`} sx={{ width: 20, display: 'inline', filter: (theme)=>theme.palette.mode==='dark'&&coinType.index===0?'invert(1)':'none' }} />
-                  <Typography variant="subtitle1" color="origin.main" flexGrow={1} textAlign="left" display='inline-flex'>{priceVal}</Typography>
-                </Stack>
-                <Typography variant="caption" sx={{color: 'text.secondary', display: 'inline'}}>≈ USD {math.round(coinUSD * priceVal, 2)}</Typography>
-              </Stack>
-          }
-            break;
-          case "buyerAddr":
-          case "sellerAddr": {
-            const addrstr = trans[column.id]
-            const dispAddress = infoByAddress[addrstr] ? infoByAddress[addrstr].name : reduceHexAddress(addrstr)
-            cellcontent = 
-              <Stack direction="row" spacing={1} alignItems="center" display="inline-flex">
-                {
-                  dispAddress.length>0 && addrstr!==blankAddress?
-                  <Link to={`/profile/others/${addrstr}`} component={RouterLink} color='inherit'>
-                    <Typography variant="body2" color="origin.main" display="inline-flex">
-                      {dispAddress}
+          case 'price':
+            {
+              const priceVal = trans.price ? math.round(trans.price / 1e18, 3) : 0;
+              const coinType = getCoinTypeFromToken(trans);
+              const coinUSD = coinPrice[coinType.index];
+              cellcontent = (
+                <Stack display="inline-flex" textAlign="left">
+                  <Stack direction="row" spacing={1}>
+                    <Box
+                      component="img"
+                      src={`/static/${coinType.icon}`}
+                      sx={{
+                        width: 20,
+                        display: 'inline',
+                        filter: (theme) =>
+                          theme.palette.mode === 'dark' && coinType.index === 0 ? 'invert(1)' : 'none'
+                      }}
+                    />
+                    <Typography
+                      variant="subtitle1"
+                      color="origin.main"
+                      flexGrow={1}
+                      textAlign="left"
+                      display="inline-flex"
+                    >
+                      {priceVal}
                     </Typography>
-                  </Link>:
-
-                  <Typography variant="body2" color="origin.main" display="inline-flex">---</Typography>
-                }
-                {
-                  infoByAddress[addrstr] && infoByAddress[addrstr].kyc && <KYCBadge/>
-                }
-              </Stack>
-          }
+                  </Stack>
+                  <Typography variant="caption" sx={{ color: 'text.secondary', display: 'inline' }}>
+                    ≈ USD {math.round(coinUSD * priceVal, 2)}
+                  </Typography>
+                </Stack>
+              );
+            }
             break;
-          case "marketTime":
-            cellcontent = <Typography variant="body2" color="text.secondary">{getDateDistance(trans.marketTime)}</Typography>
+          case 'buyerAddr':
+          case 'sellerAddr':
+            {
+              const addrstr = trans[column.id];
+              const dispAddress = infoByAddress[addrstr] ? infoByAddress[addrstr].name : reduceHexAddress(addrstr);
+              cellcontent = (
+                <Stack direction="row" spacing={1} alignItems="center" display="inline-flex">
+                  {dispAddress.length > 0 && addrstr !== blankAddress ? (
+                    <Link to={`/profile/others/${addrstr}`} component={RouterLink} color="inherit">
+                      <Typography variant="body2" color="origin.main" display="inline-flex">
+                        {dispAddress}
+                      </Typography>
+                    </Link>
+                  ) : (
+                    <Typography variant="body2" color="origin.main" display="inline-flex">
+                      ---
+                    </Typography>
+                  )}
+                  {infoByAddress[addrstr] && infoByAddress[addrstr].kyc && <KYCBadge />}
+                </Stack>
+              );
+            }
+            break;
+          case 'marketTime':
+            cellcontent = (
+              <Typography variant="body2" color="text.secondary">
+                {getDateDistance(trans.marketTime)}
+              </Typography>
+            );
             break;
           default:
             break;
@@ -112,6 +174,6 @@ const ActivityTableRow = (props) => {
         );
       })}
     </TableRow>
-  )
-}
-export default React.memo(ActivityTableRow)
+  );
+};
+export default React.memo(ActivityTableRow);
