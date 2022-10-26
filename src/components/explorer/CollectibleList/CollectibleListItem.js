abdb254b8120a92ccab0c5bd4de00636c84992af
blockchaindev91 <hisamitsuteru@gmail.com>
2022-10-26 15:55:37 +0900

    fix: update API in explorer/collectible page

--- src/components/explorer/CollectibleList/CollectibleListItem.js
+++ src/components/explorer/CollectibleList/CollectibleListItem.js
@@ -6,57 +6,67 @@ import CopyButton from '../../CopyButton';
 import { getAssetImage, getDateDistance } from '../../../utils/common';
 
 CollectibleListItem.propTypes = {
-    item: PropTypes.object.isRequired
+  item: PropTypes.object.isRequired
 };
 const TypographyStyle = styled(Typography)(({ theme }) => ({
-    [theme.breakpoints.down('sm')]: {
-        textAlign: "right"
-    }
+  [theme.breakpoints.down('sm')]: {
+    textAlign: 'right'
+  }
 }));
 export default function CollectibleListItem({ item }) {
-    const { name, createTime, royaltyOwner, tokenIdHex } = item;
-    
-    const handleErrorImage = (e) => {
-        if(e.target.src.indexOf("pasarprotocol.io") >= 0) {
-            e.target.src = getAssetImage(item, true, 1)
-        } else if(e.target.src.indexOf("ipfs.ela") >= 0) {
-            e.target.src = getAssetImage(item, true, 2)
-        } else {
-            e.target.src = '/static/broken-image.svg'
-        }
+  const { name, createTime, royaltyOwner, tokenIdHex, image } = item;
+
+  const handleErrorImage = (e) => {
+    if (e.target.src.indexOf('pasarprotocol.io') >= 0) {
+      e.target.src = getAssetImage(item, true, 1);
+    } else if (e.target.src.indexOf('ipfs.ela') >= 0) {
+      e.target.src = getAssetImage(item, true, 2);
+    } else {
+      e.target.src = '/static/broken-image.svg';
     }
-    
-    return (
-        <Stack direction="row" alignItems="center" spacing={2} sx={{p:2}}>
-            <Box
-                draggable = {false}
-                component="img"
-                alt={name}
-                src={getAssetImage(item, true)}
-                onError={handleErrorImage}
-                sx={{ width: 48, height: 48, borderRadius: 1 }}
-            />
-            <Grid container sx={{ width: (theme) => `calc(100% - ${theme.spacing(2)} - 48px)` }}>
-                <Grid item xs={8}>
-                    <Typography color="inherit" variant="subtitle2" align="left" noWrap>
-                        {name}
-                    </Typography>
-                    <Stack sx={{flexDirection: 'row'}}>
-                        <Typography variant="body2" sx={{ color: 'text.secondary', width: "auto", pt: '2px' }} align="left" noWrap>
-                            Minted By : <Link to={`/explorer/transaction/detail/${royaltyOwner}`} component={RouterLink} sx={{ color: 'text.secondary'}} onClick={(e)=>{e.stopPropagation()}}>{royaltyOwner}</Link>
-                        </Typography>
-                        <CopyButton text={royaltyOwner}/>
-                    </Stack>
-                    <Typography variant="body2" sx={{ color: 'text.secondary' }} align="left" noWrap>
-                        Token ID : {tokenIdHex}
-                    </Typography>
-                </Grid>
-                <Grid item xs={4}>
-                    <TypographyStyle variant="body2" sx={{ color: 'text.secondary' }} align="right" noWrap>
-                        {getDateDistance(createTime)}
-                    </TypographyStyle>
-                </Grid>
-            </Grid>
-        </Stack>
-    );
+  };
+
+  return (
+    <Stack direction="row" alignItems="center" spacing={2} sx={{ p: 2 }}>
+      <Box
+        draggable={false}
+        component="img"
+        alt={name}
+        src={image}
+        onError={handleErrorImage}
+        sx={{ width: 48, height: 48, borderRadius: 1 }}
+      />
+      <Grid container sx={{ width: (theme) => `calc(100% - ${theme.spacing(2)} - 48px)` }}>
+        <Grid item xs={8}>
+          <Typography color="inherit" variant="subtitle2" align="left" noWrap>
+            {name}
+          </Typography>
+          <Stack sx={{ flexDirection: 'row' }}>
+            <Typography variant="body2" sx={{ color: 'text.secondary', width: 'auto', pt: '2px' }} align="left" noWrap>
+              Minted By :{' '}
+              <Link
+                to={`/explorer/transaction/detail/${royaltyOwner}`}
+                component={RouterLink}
+                sx={{ color: 'text.secondary' }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                {royaltyOwner}
+              </Link>
+            </Typography>
+            <CopyButton text={royaltyOwner} />
+          </Stack>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }} align="left" noWrap>
+            Token ID : {tokenIdHex}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <TypographyStyle variant="body2" sx={{ color: 'text.secondary' }} align="right" noWrap>
+            {getDateDistance(createTime)}
+          </TypographyStyle>
+        </Grid>
+      </Grid>
+    </Stack>
+  );
 }
