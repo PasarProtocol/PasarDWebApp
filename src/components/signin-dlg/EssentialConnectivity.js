a8108ec8952316c4e85e2f8d05cd9eabf6b1aa03
hteru91 <hisamitsuteru@gmail.com>
2022-12-20 10:50:42 +0900

    fix: update ee connectivity

--- src/components/signin-dlg/EssentialConnectivity.js
+++ src/components/signin-dlg/EssentialConnectivity.js
@@ -3,38 +3,27 @@ import { EssentialsConnector } from '@elastosfoundation/essentials-connector-cli
 import { ApplicationDID } from '../../config';
 
 export const essentialsConnector = new EssentialsConnector();
-
 let connectivityInitialized = false;
 
-export function initConnectivitySDK() {
+export const initConnectivitySDK = () => {
   if (connectivityInitialized) return;
-
-  console.log('Preparing the Elastos connectivity SDK');
-
   // unregistear if already registerd
   const arrIConnectors = connectivity.getAvailableConnectors();
   if (arrIConnectors.findIndex((option) => option.name === essentialsConnector.name) !== -1) {
     connectivity.unregisterConnector(essentialsConnector.name);
-    console.log('unregister connector succeed.');
   }
 
   connectivity.registerConnector(essentialsConnector).then(() => {
     connectivity.setApplicationDID(ApplicationDID);
     connectivityInitialized = true;
-
-    console.log('essentialsConnector', essentialsConnector);
-    console.log('Wallet connect provider', essentialsConnector.getWalletConnectProvider());
-
     const hasLink = isUsingEssentialsConnector() && essentialsConnector.hasWalletConnectSession();
-    console.log('Has link to essentials?', hasLink);
-
     // Restore the wallet connect session - TODO: should be done by the connector itself?
     if (hasLink && !essentialsConnector.getWalletConnectProvider().connected)
       essentialsConnector.getWalletConnectProvider().enable();
   });
-}
+};
 
-export function isUsingEssentialsConnector() {
+export const isUsingEssentialsConnector = () => {
   const activeConnector = connectivity.getActiveConnector();
   return activeConnector && activeConnector.name === essentialsConnector.name;
-}
+};
